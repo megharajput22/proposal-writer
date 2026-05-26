b77ec1f9cbbe83b8ab87b2decd43ee761facd129
megharajput22 <megharajput2430@gmail.com>
2026-05-26 18:14:53 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -130,4 +130,4 @@ def generate():
         return jsonify({"proposal": "Error: " + str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
